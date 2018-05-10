5859e1f3456dc05f96a9e6a78786109bc914a2f2
sammy <sammymasoule@yahoo.dk>
2018-05-10 16:26:01 +0200

    Restclient up and running, need post highscore and logic. Login, ForgotPass & get highscore works

--- app/src/main/java/com/example/johnny/hangman/RestClient.java
+++ app/src/main/java/com/example/johnny/hangman/RestClient.java
@@ -1,17 +1,6 @@
-package com.example.johnny.hangman;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
+
+package com.example.johnny.hangman;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,115 +9,178 @@ import java.io.InputStreamReader;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
 
-/**
- * Created by sammy on 08-05-2018.
- */
 
-//This client is inspired by http://lukencode.com/2010/04/27/calling-web-services-in-android-using-httpclient/
+import android.content.Context;
+
+enum RequestMethod {
+    DELETE, GET, POST, PUT
+}
+
 public class RestClient {
-    public enum RequestMethod
-    {
-        GET,
-        POST
-    }
+
+    private boolean authentication;
+    private ArrayList<NameValuePair> headers;
+
+    private String jsonBody;
+    private String message;
+
     private ArrayList<NameValuePair> params;
-    private ArrayList <NameValuePair> headers;
+    private String response;
+    private int responseCode;
 
     private String url;
 
-    private int responseCode;
-    private String message;
+    // HTTP Basic Authentication
+    private String username;
+    private String password;
 
-    private String response;
+    protected Context context;
+
+    public RestClient(String url) {
+        this.url = url;
+        params = new ArrayList<NameValuePair>();
+        headers = new ArrayList<NameValuePair>();
+    }
 
 
-    public String getResponse() {
-        return response;
+    public void addHeader(String name, String value) {
+        headers.add(new BasicNameValuePair(name, value));
+    }
+
+    public void addParam(String name, String value) {
+        params.add(new BasicNameValuePair(name, value));
+    }
+
+    public void execute(RequestMethod method)
+            throws Exception {
+        switch (method) {
+            case GET: {
+                HttpGet request = new HttpGet(url + addGetParams());
+                request = (HttpGet) addHeaderParams(request);
+                executeRequest(request, url);
+                break;
+            }
+            case POST: {
+                HttpPost request = new HttpPost(url);
+                request = (HttpPost) addHeaderParams(request);
+                request = (HttpPost) addBodyParams(request);
+                executeRequest(request, url);
+                break;
+            }
+            case PUT: {
+                HttpPut request = new HttpPut(url);
+                request = (HttpPut) addHeaderParams(request);
+                request = (HttpPut) addBodyParams(request);
+                executeRequest(request, url);
+                break;
+            }
+            case DELETE: {
+                HttpDelete request = new HttpDelete(url);
+                request = (HttpDelete) addHeaderParams(request);
+                executeRequest(request, url);
+            }
+        }
+    }
+
+    private HttpUriRequest addHeaderParams(HttpUriRequest request)
+            throws Exception {
+        for (NameValuePair h : headers) {
+            request.addHeader(h.getName(), h.getValue());
+        }
+
+        if (authentication) {
+
+            UsernamePasswordCredentials creds = new UsernamePasswordCredentials(
+                    username, password);
+            request.addHeader(new BasicScheme().authenticate(creds, request));
+        }
+
+        return request;
+    }
+
+    private HttpUriRequest addBodyParams(HttpUriRequest request)
+            throws Exception {
+        if (jsonBody != null) {
+            request.addHeader("Content-Type", "application/json");
+            if (request instanceof HttpPost)
+                ((HttpPost) request).setEntity(new StringEntity(jsonBody,
+                        "UTF-8"));
+            else if (request instanceof HttpPut)
+                ((HttpPut) request).setEntity(new StringEntity(jsonBody,
+                        "UTF-8"));
+
+        } else if (!params.isEmpty()) {
+            if (request instanceof HttpPost)
+                ((HttpPost) request).setEntity(new UrlEncodedFormEntity(params,
+                        HTTP.UTF_8));
+            else if (request instanceof HttpPut)
+                ((HttpPut) request).setEntity(new UrlEncodedFormEntity(params,
+                        HTTP.UTF_8));
+        }
+        return request;
+    }
+
+    private String addGetParams()
+            throws Exception {
+        StringBuffer combinedParams = new StringBuffer();
+        if (!params.isEmpty()) {
+            combinedParams.append("?");
+            for (NameValuePair p : params) {
+                combinedParams.append((combinedParams.length() > 1 ? "&" : "")
+                        + p.getName() + "="
+                        + URLEncoder.encode(p.getValue(), "UTF-8"));
+            }
+        }
+        return combinedParams.toString();
     }
 
     public String getErrorMessage() {
         return message;
     }
 
+    public String getResponse() {
+        return response;
+    }
+
     public int getResponseCode() {
         return responseCode;
     }
 
-    public RestClient(String url)
-    {
-        this.url = url;
-        params = new ArrayList<NameValuePair>();
-        headers = new ArrayList<NameValuePair>();
+    public void setContext(Context ctx) {
+        context = ctx;
     }
 
-    public void AddParam(String name, String value)
-    {
-        params.add(new BasicNameValuePair(name, value));
+    public void setJSONString(String data) {
+        jsonBody = data;
     }
 
-    public void AddHeader(String name, String value)
-    {
-        headers.add(new BasicNameValuePair(name, value));
-    }
+    private void executeRequest(HttpUriRequest request, String url) {
 
-    public void Execute(RequestMethod method) throws Exception
-    {
-        switch(method) {
-            case GET:
-            {
-                //add parameters
-                String combinedParams = "";
-                if(!params.isEmpty()){
-                    combinedParams += "?";
-                    for(NameValuePair p : params)
-                    {
-                        String paramString = p.getName() + "=" + URLEncoder.encode(p.getValue(),"UTF-8");
-                        if(combinedParams.length() > 1)
-                        {
-                            combinedParams  +=  "&" + paramString;
-                        }
-                        else
-                        {
-                            combinedParams += paramString;
-                        }
-                    }
-                }
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpParams params = client.getParams();
 
-                HttpGet request = new HttpGet(url + combinedParams);
-
-                //add headers
-                for(NameValuePair h : headers)
-                {
-                    request.addHeader(h.getName(), h.getValue());
-                }
-
-                executeRequest(request, url);
-                break;
-            }
-            case POST:
-            {
-                HttpPost request = new HttpPost(url);
-
-                //add headers
-                for(NameValuePair h : headers)
-                {
-                    request.addHeader(h.getName(), h.getValue());
-                }
-
-                if(!params.isEmpty()){
-                    request.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
-                }
-
-                executeRequest(request, url);
-                break;
-            }
-        }
-    }
-
-    private void executeRequest(HttpUriRequest request, String url)
-    {
-        HttpClient client = new DefaultHttpClient();
+        // Setting 30 second timeouts
+        HttpConnectionParams.setConnectionTimeout(params, 30 * 1000);
+        HttpConnectionParams.setSoTimeout(params, 30 * 1000);
 
         HttpResponse httpResponse;
 
@@ -148,7 +200,7 @@ public class RestClient {
                 instream.close();
             }
 
-        } catch (ClientProtocolException e)  {
+        } catch (ClientProtocolException e) {
             client.getConnectionManager().shutdown();
             e.printStackTrace();
         } catch (IOException e) {
@@ -179,4 +231,3 @@ public class RestClient {
         return sb.toString();
     }
 }
-
